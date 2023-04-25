0dc32a2553c2eb5a696df52ec0fe10132d96d031
bb0134 <bb0134@uah.edu>
2023-04-25 18:56:46 +0000

    Updated historical_sim.js (added 349 lines, deleted 349 lines).

--- historical_sim.js
+++ historical_sim.js
@@ -20,7 +20,7 @@ var ponds = ee.FeatureCollection("users/kelmarkert/public/ferloPonds"),
     chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY"),
     volumne_pt = /* color: #d63000 */ee.Geometry.MultiPoint(),
     lc8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA"),
-    wendou = ee.ImageCollection("users/biplovbhandari/UAH/Wendou");
+    wendou = ee.ImageCollection("users/biplovbhandari/UAH/Wendou_2019");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Original author: K. Markert
 // Based on Soti et al. (2010) --> https://hess.copernicus.org/articles/14/1449/2010/hess-14-1449-2010.pdf
